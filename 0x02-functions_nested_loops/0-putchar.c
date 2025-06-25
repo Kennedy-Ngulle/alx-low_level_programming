626a7b7a8f3708f3aae194d903342195a0a2a228
kennedy-ngulle <kensimple56@yahoo.com>
2025-06-25 23:32:52 +0300

    included all putchar input commands

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,5 +6,17 @@
  * return (0)
  */
 
-int main (void)
+int main (void){
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+return (0);
+}
 
